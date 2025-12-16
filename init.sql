807206c28d6604df06512b06628bdbe6f29c546c
Don Locke <donaldrlocke@gmail.com>
2025-12-15 21:14:04 -0700

    Added more sql

--- init.sql
+++ init.sql
@@ -1,16 +1,32 @@
 -- Create Tables
 CREATE TABLE branches (
     branch_id INT PRIMARY KEY, 
-    branch_address text
+    branch_address TEXT
+);
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name TEXT
 );
 
 CREATE TABLE boxes (
+    box_id INT PRIMARY KEY,
     box_number INT,
     branch_id INT, 
-    PRIMARY KEY (box_number, branch_id),
     FOREIGN KEY (branch_id) REFERENCES branches (branch_id)
+);
+
+CREATE TYPE box_event AS ENUM ('opened', 'closed');
+
+CREATE TABLE box_history (
+    box_number INT,
+    event_type box_event,
+    event_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (box_number, event_date),
+    FOREIGN KEY (box_number) REFERENCES boxes (box_id)
 );
 
 -- Add Data
 INSERT INTO branches (branch_id, branch_address) VALUES (1, '1 Seneca St. Buffalo NY, 14216');
-INSERT INTO boxes (box_number, branch_id) VALUES (1, 1);
+INSERT INTO boxes (box_id, box_number, branch_id) VALUES (1, 1, 1);
+INSERT INTO box_history (box_number, event_type) VALUES (1, 'opened');
