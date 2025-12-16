a4a176f76bdfd82416a404b3663d5aa1fa86014f
sydneybrown <sydneykaylabrown@gmail.com>
2025-12-16 13:59:44 -0500

    Redirect to login

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,11 @@
 import type { PageServerLoad } from "./$types";
+import { redirect } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ locals }) => {
-
+if(!locals.user)
+{
+  throw redirect(303, '/login');
+}
   const response = await locals.postgres?.query("SELECT customer_id, customer_name, customer_username FROM customers WHERE customer_id = $1", [locals.user]);
   const user = response?.rows[0];
   return { user: user };
