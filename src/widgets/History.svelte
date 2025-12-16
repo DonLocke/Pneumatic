e09fed90cae7127c6e8f4b39bb29a1402575319a
Greg Cherry <Gcherry2@gmail.com>
2025-12-16 13:36:55 -0500

    history loop

--- src/widgets/History.svelte
+++ src/widgets/History.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  export let boxID;
-  // const boxID = data.box.id;
+  export let boxHistory;
+  console.log(boxHistory);
+  //const boxID = boxHistory.box_id;
   // const branch = data.box.branch;
   // const address = data.box.address;
 </script>
@@ -12,7 +13,7 @@
         <div class="grid">
           <div class="cell">
             <p class="title is-4">Box Number</p>
-            <!-- <p class="subtitle is-4">{branch}</p> -->
+            <!-- <p class="subtitle is-4">test</p> -->
           </div>
           <div class="cell">
             <p class="title is-4">Box Event</p>
@@ -22,6 +23,20 @@
             <p class="title is-4">Box Event Time</p>
             <!-- <p class="subtitle is-4">{address}</p> -->
           </div>
+          {#each boxHistory as history}
+            <div class="cell">
+              <p class="title is-4">{history.box_number}</p>
+              <!-- <p class="subtitle is-4">test</p> -->
+            </div>
+            <div class="cell">
+              <p class="title is-4">blah</p>
+              <!-- <p class="subtitle is-4">{address}</p> -->
+            </div>
+            <div class="cell">
+              <p class="title is-4">Box Event Time</p>
+              <!-- <p class="subtitle is-4">{address}</p> -->
+            </div>
+          {:else}{/each}
         </div>
       </div>
     </div>
