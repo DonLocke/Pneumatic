60d3b1f78db2b236136995ca58813789cb357b23
Kevin Baker <kevinrandallbaker@gmail.com>
2025-12-16 15:53:40 -0500

    started conver

--- src/widgets/payment.svelte
+++ src/widgets/payment.svelte
@@ -6,7 +6,49 @@
   const date = new Date(paymentInfo.payment_date).toLocaleDateString();
 </script>
 
-<div class="box">
+<article class="message is-large">
+  <div class="message-header">Payment Information</div>
+  <div class="message-body">
+    <div class="fixed-grid">
+      <div class="grid">
+        <div class="cell">
+          <p class="title is-4">Status</p>
+          <p class="subtitle is-4">{boxInfo.payment_status}</p>
+        </div>
+        <div class="cell">
+          <p class="title is-4">Last Payment</p>
+          <p class="subtitle is-4">Amount: {paymentInfo.payment_amount}</p>
+          <p class="subtitle is-4">Date: {date}</p>
+        </div>
+        <div class="cell">
+          <p class="title is-4">Payment History</p>
+          <table class="table">
+            <thead>
+              <tr>
+                <th><appr title="BoxNumber">Amount</appr></th>
+                <th><appr title="BoxEvent">Date</appr></th>
+              </tr>
+            </thead>
+            <tbody>
+              {#each paymentHistory as history}
+                <tr>
+                  <td>
+                    {history.payment_amount}
+                  </td>
+                  <td>
+                    {history.payment_date}
+                  </td>
+                </tr>
+              {:else}{/each}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</article>
+
+<!-- <div class="box">
   <h1 class="title">Payment Information</h1>
   <div class="grid">
     <div class="cell">
@@ -42,4 +84,4 @@
       </table>
     </div>
   </div>
-</div>
+</div> -->
