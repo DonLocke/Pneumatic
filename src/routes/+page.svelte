ec5cf9c09f7b80cd742025aaf57994ce61bdf93b
Don Locke <donaldrlocke@gmail.com>
2025-12-16 18:17:47 -0700

    Add new box to outlined

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,12 +43,21 @@
         </a>
       </div>
       {/each}
+      
       {#if data.boxes.length < 2}
       <div class="cell">
-        <div style="outline: 2px dashed grey; border-radius: 5px; height: 100%; ">
+        <div class="level" style="outline: 2px dashed grey; border-radius: 5px; height: 100%;">
+          <div class="level-item has-text-centered">
+            <a class="button is-primary is-inverted is-rounded is-large" aria-label="Open New Box" href="/new-box">
+              <div class="icon has-text-centered">
+                <i class="fa fa-plus fa-2x"></i>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       {/if}
+
       {#if data.boxes.length < 3}
       <div class="cell">
         <div style="outline: 2px dashed grey; border-radius: 5px; height: 100%; ">
