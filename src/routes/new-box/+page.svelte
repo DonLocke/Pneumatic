73374a48d5e963daaf51fb858a61db70ba532405
Don Locke <donaldrlocke@gmail.com>
2025-12-17 12:48:08 -0700

    add select new box

--- src/routes/new-box/+page.svelte
+++ src/routes/new-box/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-  export let data;
+  import type { PageData } from "./$types";
 
-  let showModal = false;
-  let tier = "";
+  let { data } = $props<{data: PageData}>();
+
+  let showModal = $state(false);
+  let tier = $state("");
+  let selected_branch = $state("");
 
   function toggleModal(event: MouseEvent) {
     if (showModal) {
@@ -12,8 +15,6 @@
       tier = (event.currentTarget as HTMLButtonElement).value;
       showModal = true;
     }
-
-    console.log(tier);
   }
 </script>
 
@@ -82,14 +83,14 @@
   <div class="modal-card">
     <header class="modal-card-head">
       <p class="modal-card-title has-text-primary">{tier}</p>
-      <button class="delete" aria-label="close" onclick={toggleModal}></button>
+      <button class="delete" aria-label="close" type="reset" onclick={toggleModal}></button>
     </header>
     <section class="modal-card-body">
       <div class="field">
         <div class="label">Location</div>
         <div class="control">
           <div class="select is-fullwidth">
-            <select>
+            <select bind:value={selected_branch}>
               {#each data.branches as branch}
                 <option value={branch.branc_id}>{branch.branch_name}</option>
               {/each}
@@ -97,12 +98,49 @@
           </div>
         </div>
       </div>
+
+      {#if selected_branch != ""}
+      <div class="container mt-5">
+        <div class="field">
+          <div class="label">Choose Your Box</div>
+          <div class="fixed-grid has-12-cols">
+            <div class="grid ">
+              {#each {length: 72} as _, index}
+              <div class="cell">
+                <button aria-label="Box #${index}">
+                  <div
+                    class="icon has-text-primary"
+                    class:has-text-primary={
+                      (tier == "Sprint" && (index % 4 == 0 || index % 7 == 0)) ||
+                      (tier == "Standard" && (index % 2 == 0 || index % 15 == 0)) ||
+                      (tier == "Marathon" && (index % 10 == 0 || index % 19 == 0))
+                    }
+                    class:has-text-danger={
+                      (tier == "Sprint" && (index % 9 == 0 || index % 13 == 0)) ||
+                      (tier == "Standard" && (index % 17 == 0 || index % 18 == 0)) ||
+                      (tier == "Marathon" && (index % 6 == 0 || index % 21 == 0))
+                    }
+                    ><i class="fa fa-square fa-2x"></i>
+                  </div>
+                </button>
+              </div>
+              {/each}
+            </div>
+          </div>
+        </div>
+      </div>
       
+      <div class="field mt-5">
+        <div class="label is-1">Total</div>
+        <p class="subtitle">$25.99</p>
+      </div>
+      {/if}
+
     </section>
     <footer class="modal-card-foot">
       <div class="buttons">
-        <button class="button is-success" onclick={toggleModal}>Submit</button>
-        <button class="button" onclick={toggleModal}>Cancel</button>
+        <button class="button is-success" onclick={toggleModal} disabled={selected_branch == ""}>Submit</button>
+        <button class="button" type="reset" onclick={toggleModal}>Cancel</button>
       </div>
     </footer>
   </div>
